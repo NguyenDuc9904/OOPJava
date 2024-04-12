1b3b5484ab06335a357744ff4316c41afd765fac
NguyenDuc9904 <gotukun@gmail.com>
2024-04-12 11:01:49 +0700

    nop bt ngay 10/4

--- baithuchanh/src/util/Configs.java
+++ baithuchanh/src/util/Configs.java
@@ -1,8 +1,8 @@
 package util;
 
 public class Configs {
-    public static int NHAN_VIEN_SEP;
-    public static int NHAN_VIEN_LINH;
+    public static int NHAN_VIEN_SEP=0;
+    public static int NHAN_VIEN_LINH=1;
     final public static long LUONG_NHAN_VIEN_FULL_TIME_SEP=20000000;
     final public static long LUONG_NHAN_VIEN_FULL_TIME_LINH=10000000;
     final public static long LUONG_LAM_THEM_MOI_NGAY=800000;
